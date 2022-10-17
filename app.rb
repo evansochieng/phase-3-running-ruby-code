fb54f56a71352b76765bdd66eeaca31771aba267
Evans Ochieng <evansochieng121@gmail.com>
2022-10-17 11:43:20 +0300

    Running my first ruby code, terminal output methods

---
+++ app.rb
@@ -0,0 +1,18 @@
+# # puts adds a line break
+# puts "Hello world!"
+# puts("Excited to start learning Ruby :)") #adding () is optional
+
+# # print doesn't add a line break
+# print "Hello world!"
+# print "Hello world!"
+
+# # Displaying non-string objects
+# p [1, 2, 3]
+
+# # Pretty-printing complex data
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+# Tests
+puts "Hello World!"
+print "Pass this test, please."
+p [1, 2, 3]
